504228d09c03e7aa6d33d7944d7ebb83199b2feb
Sebastian-IngSoft <sebastiansarmientocas@gmail.com>
2023-12-15 21:29:15 -0500

    iniciando blog

--- database/migrations/2023_12_16_015602_create_profiles_table.php
+++ database/migrations/2023_12_16_015602_create_profiles_table.php
@@ -11,13 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->string('title',45);
+            $table->text('biografia');
+            $table->string('website',45);
+            $table->unsignedBigInteger('user_id')->unique();//relacion uno a uno
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles');
     }
 };
